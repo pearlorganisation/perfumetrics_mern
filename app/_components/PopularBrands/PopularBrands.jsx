af48636c50a793d590389685461ae93609d51b47
jai <jaix2793@gmail.com>
2024-09-08 22:25:19 +0530

    updates

--- app/_components/PopularBrands/PopularBrands.jsx
+++ app/_components/PopularBrands/PopularBrands.jsx
@@ -22,9 +22,9 @@ const PopularBrands = () => {
   }, [])
 
 
-  useEffect(() => {
-    console.log("popularPerfumeData", popularPerfumeData);
-  }, [popularPerfumeData])
+  useEffect(()=>{
+    console.log("popularPerfumeData",popularPerfumeData);
+  },[popularPerfumeData])
 
   return (
     <>
@@ -42,13 +42,13 @@ const PopularBrands = () => {
 
                   <section className=" h-[200px] w-[200px] md:h-[200px] md:w-[200px] xl:w-[240px] xl:h-[240px] lg:w-[240px] lg:h-[240px] flex flex-col justify-center items-center ">
                     <div className="h-[80px] w-[80px] md:h-[120px] md:w-[120px] xl:w-[150px] xl:h-[150px] lg:w-[150px] lg:h-[150px] rounded-full overflow-hidden shadow-[0_0_0_5px#f193c4] flex-shrink-0">
-                      <img
-                        src={item.banner}
-                        className="w-full h-full object-cover p-1 rounded-full hover:scale-125 ease-in duration-300"
-                      />
+                    <img
+                      src={item.banner}
+                      className="w-full h-full object-cover p-1 rounded-full hover:scale-125 ease-in duration-300"
+                    />
                     </div>
-
-                    <div className="mt-4"><b>{item.perfume}</b><br />By {item?.brand?.brand || 'redo'}</div>
+                    
+                    <div className="mt-4"><b>{item.perfume}</b><br/>By {item?.brand?.brand||'redo'}</div>
                   </section>
                 );
               })}
